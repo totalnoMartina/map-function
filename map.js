826c2dd4ec343e81fa439cb01b6019a29a856175
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-22 23:31:44 +0000

    Introduction map method

--- map.js
+++ map.js
@@ -1,39 +1,54 @@
-  
-/**
- * To run this file in Gitpod, use the 
- * command node map.js in the terminal
- */
+  /**
+   * To run this file in Gitpod, use the 
+   * command node map.js in the terminal
+   */
 
-// Using a for loop
+  // Using a for loop
+  let nums = [1, 2, 3, 4, 5];
+  let results = [];
 
+  for (let num of nums) {
+      results.push(num * 2);
+  }
+  console.log(results)
 
-// Using map()
+  // Using map()
+  const multiByTwo = function(num) {
+      return num * 2
+  }
+  const mapResults = nums.map(multiByTwo);
+  console.log(mapResults);
+  // Simplified w/ map()
 
+  const simplify = nums.map(function(num) {
+      return num * 2
+  });
 
-// Simplified w/ map()
+  console.log(simplify);
+  // Simplfied w/ map() + arrow function
+  const moreSimple = nums.map(num => num * 2);
+  console.log(moreSimple);
 
+  // With objects:
+  const students = [{
+          id: 1,
+          name: 'Mark',
+          profession: 'Developer',
+          skill: 'JavaScript'
+      },
+      {
+          id: 2,
+          name: 'Ariel',
+          profession: 'Developer',
+          skill: 'HTML'
+      },
+      {
+          id: 3,
+          name: 'Jason',
+          profession: 'Designer',
+          skill: 'CSS'
+      },
+  ];
 
-// Simplfied w/ map() + arrow function
-
-
-// With objects:
-const students = [
-  {
-    id: 1,
-    name: 'Mark',
-    profession: 'Developer',
-    skill: 'JavaScript'
-  },
-  {
-    id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
-    skill: 'HTML'
-  },
-  {
-    id: 3,
-    name: 'Jason',
-    profession: 'Designer',
-    skill: 'CSS'
-  },
-];
+  const studentsWithIds = students.map(students => [students.name, students.id, ]);
+  console.log(studentsWithIds);
